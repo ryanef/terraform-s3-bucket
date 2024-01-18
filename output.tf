e5fab651b39963a1ad97b191287b157cbb2fc615
ryan-wsl <ryanef39@gmail.com>
2024-01-18 17:30:31 -0500

    output updates

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "s3_bucket_id" {
 
-  value       = aws_s3_bucket.bucket.*.id
+  value       = [for bucket in aws_s3_bucket.bucket: bucket.id]
 }
 
 output "s3_bucket_arn" {
@@ -8,12 +8,12 @@ output "s3_bucket_arn" {
   value       = try(aws_s3_bucket.bucket.*.arn, "")
 }
 
-output "s3_bucket_bucket_regional_domain_name" {
+output "s3_bucket_regional_domain_name" {
 
-  value       = aws_s3_bucket.bucket.*.bucket_regional_domain_name
+  value       = try([for bucket in aws_s3_bucket.bucket: bucket.bucket_regional_domain_name], []) 
 }
 
-output "s3_bucket_bucket_domain_name" {
+output "s3_bucket_domain_name" {
   description = "formatted like: bucketname.s3.amazonaws.com."
-  value       = aws_s3_bucket.bucket.*.bucket_domain_name
+  value       = try([for bucket in aws_s3_bucket.bucket: bucket.bucket_domain_name], []) 
 }
