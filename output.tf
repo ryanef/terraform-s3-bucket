b408efee7c41e45c0169124ee5236711719afd8e
ryan-wsl <ryanef39@gmail.com>
2024-01-19 11:11:54 -0500

    output changes

--- output.tf
+++ output.tf
@@ -1,19 +1,20 @@
 output "s3_bucket_id" {
 
-  value       = [for k,v in aws_s3_bucket.bucket: "${k}"]
+  value       = {for key, value in aws_s3_bucket.bucket: key=>"${value.id}" }
 }
+
 
 output "s3_bucket_arn" {
 
-  value       = try(aws_s3_bucket.bucket.*.arn, "")
+  value       = try({for key, value in aws_s3_bucket.bucket: key=>"${value.arn}" }, {}) 
 }
 
 output "s3_bucket_regional_domain_name" {
 
-  value       = try([for bucket in aws_s3_bucket.bucket: bucket.bucket_regional_domain_name], []) 
+  value       = try({for key, value in aws_s3_bucket.bucket: key=>"${value.bucket_regional_domain_name}"}, {}) 
 }
 
 output "s3_bucket_domain_name" {
   description = "formatted like: bucketname.s3.amazonaws.com."
-  value       = try([for bucket in aws_s3_bucket.bucket: bucket.bucket_domain_name], []) 
+  value       = try({for key, value in aws_s3_bucket.bucket: key=>"${value.bucket_domain_name}"}, {}) 
 }
