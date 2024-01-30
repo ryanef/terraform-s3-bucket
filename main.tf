c058122e0e4f0df7260c436cb6d8d9e6030963b3
ryan-wsl <ryanef39@gmail.com>
2024-01-30 13:16:54 -0500

    outputs

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 data "aws_region" "current" {}
 
 data "aws_canonical_user_id" "this" {
-
 }
 
 data "aws_caller_identity" "current" {}
--- output.tf
+++ output.tf
@@ -1,20 +1,20 @@
 output "s3_bucket_id" {
 
-  value       = aws_s3_bucket.bucket.*.id
+  value       = aws_s3_bucket.bucket.id
 }
 
 
 output "s3_bucket_arn" {
 
-  value       = aws_s3_bucket.bucket.*.arn
+  value       = aws_s3_bucket.bucket.arn
 }
 
 output "s3_bucket_regional_domain_name" {
 
-  value       = aws_s3_bucket.bucket.*.bucket_regional_domain_name
+  value       = aws_s3_bucket.bucket.bucket_regional_domain_name
 }
 
 output "s3_bucket_domain_name" {
   description = "formatted like: bucketname.s3.amazonaws.com."
-  value       = aws_s3_bucket.bucket.*.bucket_domain_name 
+  value       = aws_s3_bucket.bucket.bucket_domain_name 
 }
